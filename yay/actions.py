7df038972d59c4d6e626acef852b0831b3994712
John Carr <john.carr@isotoma.com>
2010-12-27 11:52:31 +0000

    Add a CopyResolver that lets us delay copies to the final resolve() step, but we are going to have to delay everything to fix up the now failing tests

--- yay/actions.py
+++ yay/actions.py
@@ -1,11 +1,21 @@
 
 import copy
+from yay.resolver import IResolvable
 
 class IAction(object):
 
-    def run(self, mgr, current_value, action_data, *action_args):
+    def run(self, current_value, action_data, *action_args):
         """ Given the current value of a parameter and some data, apply a transformation """
         pass
+
+
+class ResolvableAction(IAction):
+    """ I am an action that simply returns a Resolvable subclass (something that is delayed to the resolver runs) """
+
+    resolvable = None
+
+    def run(self, current_value, action_data, **action_args):
+        return self.resolvable(current_value, action_data, **action_args) 
 
 
 class Actions(object):
@@ -13,8 +23,13 @@ class Actions(object):
     def __init__(self, config):
         self.config = config
         self.actions = {}
-        for cls in IAction.__subclasses__():
-            self.actions[cls.name] = cls()
+        def find_subclasses(cls):
+            for node in cls.__subclasses__():
+                if not hasattr(node, "name"):
+                    find_subclasses(node)
+                    continue
+                self.actions[node.name] = node()
+        find_subclasses(IAction)
 
     def lookup(self, key):
         return self.config._raw.get(key, None)
@@ -22,7 +37,7 @@ class Actions(object):
     def run(self, action, current_value, action_data):
         if not action in self.actions:
             raise KeyError("'%s' is not a supported action" % action)
-        return self.actions[action].run(self, current_value, action_data)
+        return self.actions[action].run(current_value, action_data)
 
 
 class Assign(IAction):
@@ -30,7 +45,7 @@ class Assign(IAction):
 
     name = "assign"
 
-    def run(self, mgr, current_value, action_data, *action_args):
+    def run(self, current_value, action_data, *action_args):
         """ Just return action_data to replace current value entirely """
         return action_data
 
@@ -40,7 +55,7 @@ class Append(IAction):
 
     name = "append"
 
-    def run(self, mgr, current_value, action_data, *action_args):
+    def run(self, current_value, action_data, *action_args):
         # FIXME: Ensure that current value is a list
         # FIXME: Ensure that action data is a list
         return current_value + action_data
@@ -51,24 +66,25 @@ class Remove(IAction):
 
     name = "remove"
 
-    def run(self, mgr, current_value, action_data, *action_args):
+    def run(self, current_value, action_data, *action_args):
         # FIXME: Ensure action_data is a list or supports __contains__
         if current_value is None:
             return []
         return [x for x in current_value if x not in action_data]
 
-class Copy(IAction):
-    """ Inherit a section, copy a literal etc """
 
-    name = "copy"
+class CopyResolver(IResolvable):
 
-    def run(self, mgr, current_value, action_data, *action_args):
+    def resolve(self, data):
+        # Make sure full chain is resolved
+        super(CopyResolver, self).resolve(data)
+
         # This is derived from string.py Formatter so lookup language
         # is consistent
         # field name parser is written in C and available as
         #   str._formatter_field_name_split()
-        first, rest = action_data._formatter_field_name_split()
-        obj = mgr.lookup(first)
+        first, rest = self.action_data._formatter_field_name_split()
+        obj = data[first]
 
         for is_attr, i in rest:
             if is_attr:
@@ -77,4 +93,10 @@ class Copy(IAction):
                 obj = obj[i]
 
         return copy.deepcopy(obj)
+
+class Copy(ResolvableAction):
+    """ Inherit a section, copy a literal etc """
+
+    name = "copy"
+    resolvable = CopyResolver
 
--- yay/resolver.py
+++ yay/resolver.py
@@ -1,10 +1,27 @@
 
+class IResolvable(object):
+    """ Something that we dont want to do until the Resolver().resolve() stage """
+    #FIXME: There is a bit of conflation between resolver and actions that needs ironing out
+    #FIXME: This is no longer an interface
+    def __init__(self, current_value, action_data, **action_args):
+        self.current_value = current_value
+        self.action_data = action_data
+        self.action_args = action_args
+
+    def resolve(self, data):
+        #FIXME: needs cycle detection
+        while isinstance(self.current_value, IResolvable):
+            self.current_value = self.current_value.resolve(data)
+
 class Resolver(object):
 
     """ Run .format() on all strings in a dictionary, raises a ValueError on cycles """
 
     def __init__(self, raw):
         self._raw = raw
+
+    def resolve_resolvable(self, resolvable):
+        return resolvable.resolve(self._raw)
 
     def resolve_value(self, value, label=""):
         if isinstance(value, basestring):
@@ -13,6 +30,8 @@ class Resolver(object):
             return self.resolve_dict(value, label)
         elif isinstance(value, list):
             return self.resolve_list(value, label)
+        elif isinstance(value, IResolvable):
+            return self.resolve_resolvable(value)
         return value
 
     def resolve_string(self, value, label=""):
