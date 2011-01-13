1988ff3db74596ab793fff14067aa6be19d180cf
John Carr <john.carr@isotoma.com>
2011-01-13 18:56:58 +0000

    Mapping test

--- yay/tests/test_nodes.py
+++ yay/tests/test_nodes.py
@@ -26,6 +26,19 @@ class TestSequenceOperations(unittest.TestCase):
 
         self.failUnlessEqual(r.resolve(self.ctx), [1, 3])
 
+
+class TestMapping(unittest.TestCase):
+
+    def setUp(self):
+        self.ctx = Context(None, None)
+
+    def test_get_set(self):
+        d = Mapping(None)
+        d.set("foo", Boxed(1))
+
+        self.failUnlessEqual(d.get(None, "foo", None).value, 1)
+
+
 class TestLookup(unittest.TestCase):
 
     def setUp(self):
@@ -37,3 +50,4 @@ class TestLookup(unittest.TestCase):
         l = Lookup(d, Boxed("foo"))
  
         self.failUnlessEqual(l.resolve(self.ctx), 1)
+
