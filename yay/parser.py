68a157ce211d42587137c41bde593e6a2eab1510
John Carr <john.carr@isotoma.com>
2011-01-16 00:28:07 +0000

    Add some parseActions to build correct expression tree nodes

--- yay/parser.py
+++ yay/parser.py
@@ -13,7 +13,9 @@
 # limitations under the License.
 
 from yay.pyparsing import Forward, Group, Keyword, Optional, Word, \
-    ZeroOrMore, oneOf, alphas, nums, alphanums, Combine
+    ZeroOrMore, oneOf, alphas, nums, alphanums, Combine, Suppress
+
+from yay.nodes import expression as nodes
 
 AND = Keyword("and")
 OR = Keyword("or")
@@ -22,29 +24,55 @@ BINOP = oneOf("= != < > <= >=")
 
 identifier = Word(alphanums+"_")
 arithSign = Word("+-",exact=1)
-intNum = Combine( Optional(arithSign) + Word( nums ) )
+intNum = Combine( Optional(arithSign) + Word( nums ) ).setParseAction(lambda s, w, t: int(t[0]))
 
 expression = Forward()
 
 filterExpression = Forward()
+
+def filter_bin_comparison_action(s, w, t):
+    cls = {
+        "=": nodes.Equal,
+        "!=": nodes.NotEqual,
+        "<": nodes.LessThan,
+        "<=": nodes.LessThanEqual,
+        ">": nodes.GreaterThan,
+        ">=": nodes.GreaterThanEqual,
+        }[t[0][1]]
+    return cls(t[0][0], t[0][2])
+filter_bin_comparison = Group(expression + BINOP + expression)
+filter_bin_comparison.setParseAction(filter_bin_comparison_action)
+
 filterCondition = Group(
-    ( expression + BINOP + expression ) |
+    filter_bin_comparison |
     ( "(" + filterExpression + ")" )
     )
-filterExpression << filterCondition + ZeroOrMore((AND|OR) + filterExpression)
 
-listAccess = "[" + (
+def filter_expression_action(s, w, t):
+    node = t[0]
+    for i in range(1, len(t)):
+        if t[i] == "and":
+            node = nodes.And(node, t[i+1])
+        elif t[i] == "or":
+            node = nodes.Or(node, t[i+1])
+        i += 1
+    return node
+filterExpression << filterCondition + ZeroOrMore((AND|OR) + filterExpression)
+filterExpression.setParseAction(filter_expression_action)
+
+listAccess = Suppress("[") + (
     filterExpression |
     intNum
-    ) + "]"
+    ) + Suppress("]")
 
 fullExpression = identifier + ZeroOrMore(
     listAccess |
-    "." + identifier
+    Suppress(".") + identifier
     )
 
-expression << (
-    fullExpression |
-    intNum
+expression << Group(
+    intNum |
+    fullExpression
     )
 
+print fullExpression.parseString("foo.bar[foo.age < 12 and foo.badger > 5][0]")
