e02636dd5de9a64d986fe77763c3801af4f7247e
John Carr <john.carr@isotoma.com>
2011-06-28 23:36:09 +0100

    Add a way of holding strings with secrets in them

---
+++ yay/protectedstring.py
@@ -0,0 +1,51 @@
+# Copyright 2010-2011 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class ProtectedStringPart(object):
+    """ I represent a portion of a string which may or may not be sensitive """
+
+    __slots__ = ("value", "secret", )
+
+    def __init__(self, value, secret):
+        self.value = value
+        self.secret = secret
+
+    @property
+    def protected(self):
+        if self.secret:
+            return "*" * 5
+        return self.value
+
+    @property
+    def unprotected(self):
+        return self.value
+
+
+class ProtectedString(unicode):
+    """ I represent a string which contains sensitive parts """
+
+    def __init__(self, parts):
+        self.parts = parts
+
+    def __str__(self):
+        return "".join([str(p.protected) for p in self.parts])
+
+    def __unicode__(self):
+        return u"".join([unicode(p.protected) for p in self.parts])
+
+    @property
+    def unprotected(self):
+        return u"".join([unicode(p.unprotected) for p in self.parts])
+
---
+++ yay/tests/test_protectedstring.py
@@ -0,0 +1,38 @@
+# Copyright 2010-2011 Isotoma Limited
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import unittest
+from yay.protectedstring import *
+
+
+class TestProtectedString(unittest.TestCase):
+
+    def test_normal_string(self):
+        a = ProtectedStringPart("Just a normal string", False)
+        b = ProtectedString([a])
+
+        self.failUnlessEqual(unicode(b), "Just a normal string")
+        self.failUnlessEqual(b.unprotected, "Just a normal string")
+
+    def test_partially_secret(self):
+        a = ProtectedStringPart("adduser -u fred -p ", False)
+        b = ProtectedStringPart("password", True)
+        c = ProtectedStringPart(" -h /home/fred", False)
+        d = ProtectedString([a,b,c])
+
+        self.failUnlessEqual(unicode(d), "adduser -u fred -p ***** -h /home/fred")
+        self.failUnlessEqual(d.unprotected, "adduser -u fred -p password -h /home/fred")
+
+
