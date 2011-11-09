97be6bf767dafd2953a39b1a089591319f169c54
John Carr <john.carr@isotoma.com>
2011-11-09 23:30:29 +0000

    Support a search path

--- yay/openers.py
+++ yay/openers.py
@@ -12,10 +12,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import urllib
+import urllib, urlparse
 import StringIO
 import os
 import subprocess
+
+from yay.errors import NotFound
+
 
 class IOpener(object):
 
@@ -78,27 +81,53 @@ class Gpg(object):
         stream.secret = True
         return stream
 
+
 class Openers(object):
 
-    def __init__(self):
+    def __init__(self, searchpath=None):
+        self.searchpath = searchpath or []
+
         self.openers = {}
         for cls in IOpener.__subclasses__():
             self.openers[cls.scheme] = cls()
 
+    def _scheme(self, uri):
+        parsed = urlparse.urlparse(uri)
+        return parsed.scheme
+
+    def _absolute(self, uri):
+        return self._scheme(uri) or uri.startswith("/")
+
+    def _relative(self, uri):
+        return not self._absolute(uri)
+
+    def _join(self, *uri):
+        if self._scheme(uri[0]):
+            return urlparse.urljoin(*uri)
+        return os.path.join(*uri)
+
+    def _open(self, uri):
+        for scheme, opener in self.openers.iteritems():
+            if uri.startswith(scheme):
+                return opener.open(uri)
+
     def open(self, uri):
         fp = None
 
-        for scheme, opener in self.openers.iteritems():
-            if uri.startswith(scheme):
-                fp = opener.open(uri)
-                break
+        if self._absolute(uri):
+            fp = self._open(uri)
         else:
-            # Support direct file paths that dont specify a scheme
-            if os.path.exists(uri):
-                fp = self.openers["file://"].open(uri)
+            for path in self.searchpath:
+                fp = self._open(self._join(path, uri))
+                if fp:
+                    break
+            else:
+                # Support direct file paths that dont specify a scheme
+                if os.path.exists(uri):
+                    fp = self.openers["file://"].open(uri)
 
         if not fp:
-            raise ValueError("URI '%s' cannot be opened (unsupported scheme or missing local file)" % uri)
+            raise NotFound("'%s' could not be found" % uri)
 
         if uri.endswith(".gpg"):
             fp = Gpg().filter(fp)
