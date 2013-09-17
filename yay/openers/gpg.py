cc0a832bdf4c63953104077ae4f282c35f541188
John Carr <john.carr@unrouted.co.uk>
2013-09-17 21:50:28 +0100

    Remove gpglib stuff for now

--- yay/openers/gpg.py
+++ yay/openers/gpg.py
@@ -18,61 +18,41 @@ import subprocess
 from yay.compat import io
 from yay.errors import NotFound, NotModified, ParadoxError
 
-try:
-    from gpglib.structures import EncryptedMessage, Key
-    import this_definitely_doesnt_exist_we_need_passphrase_ui_before_we_can_do_this
 
-    class Gpg(object):
-        def filter(self, fp):
-            key = Key(passphrase='password')
-            key.parse(open(os.path.expanduser("~/.gnupg/secring.gpg")).read())
+class Gpg(object):
+    def get_gpg_binary(self):
+        # FIXME: Memoize me
+        for binary in ('gpg2', 'gpg'):
+            for path in os.environ.get('PATH', '/usr/bin').split(":"):
+                t = os.path.join(path, binary)
+                if os.path.exists(t):
+                    return t
+        raise NotFound("Unable to decrypt GPG encrypted resource as could not find 'gpg' or 'gpg2'")
 
-            decryptor = EncryptedMessage(key.key_dict())
-            decryptor.decrypt(fp.read())
+    def filter(self, fp):
+        data = fp.read()
 
-            stream = io.StringIO(decryptor.plaintext)
-            stream.etag = fp.etag
-            stream.len = len(decryptor.plaintext)
-            stream.uri = fp.uri
-            stream.labels = ('secret', )
-            return stream
+        # Build an environment for the child process
+        # In particular, if GPG_TTY is not set then gpg-agent will not prompt
+        # for a passphrase even it is running and correctly configured.
+        # GPG_TTY is not required if using seahorse-agent.
+        # This is not used on OSX as there is no /proc/self/fd/0
+        env = os.environ.copy()
+        if not "GPG_TTY" in env:
+            if os.path.exists("/proc/self/fd/0"):
+                env['GPG_TTY'] = os.readlink('/proc/self/fd/0')
 
+        p = subprocess.Popen([self.get_gpg_binary(), "--use-agent", "--batch", "-d"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, env=env)
+        stdout, stderr = p.communicate(data)
+        if p.returncode != 0:
+            msg = "Unable to decrypt resource '%s'" % fp.uri
+            if not "GPG_AGENT_INFO" in os.environ:
+                msg += "\nGPG Agent not running so your GPG key may not be available"
+            raise NotFound(msg)
 
-except ImportError:
-    class Gpg(object):
-        def get_gpg_binary(self):
-            # FIXME: Memoize me
-            for binary in ('gpg2', 'gpg'):
-                for path in os.environ.get('PATH', '/usr/bin').split(":"):
-                    t = os.path.join(path, binary)
-                    if os.path.exists(t):
-                        return t
-            raise NotFound("Unable to decrypt GPG encrypted resource as could not find 'gpg' or 'gpg2'")
-
-        def filter(self, fp):
-            data = fp.read()
-
-            # Build an environment for the child process
-            # In particular, if GPG_TTY is not set then gpg-agent will not prompt
-            # for a passphrase even it is running and correctly configured.
-            # GPG_TTY is not required if using seahorse-agent.
-            # This is not used on OSX as there is no /proc/self/fd/0
-            env = os.environ.copy()
-            if not "GPG_TTY" in env:
-                if os.path.exists("/proc/self/fd/0"):
-                    env['GPG_TTY'] = os.readlink('/proc/self/fd/0')
-
-            p = subprocess.Popen([self.get_gpg_binary(), "--use-agent", "--batch", "-d"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, env=env)
-            stdout, stderr = p.communicate(data)
-            if p.returncode != 0:
-                msg = "Unable to decrypt resource '%s'" % fp.uri
-                if not "GPG_AGENT_INFO" in os.environ:
-                    msg += "\nGPG Agent not running so your GPG key may not be available"
-                raise NotFound(msg)
-
-            stream = io.StringIO(stdout)
-            stream.etag = fp.etag
-            stream.len = len(stdout)
-            stream.uri = fp.uri
-            stream.labels = ('secret', )
-            return stream
+        stream = io.StringIO(stdout)
+        stream.etag = fp.etag
+        stream.len = len(stdout)
+        stream.uri = fp.uri
+        stream.labels = ('secret', )
+        return stream
